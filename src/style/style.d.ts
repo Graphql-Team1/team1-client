622eb801fee4c5a9186be57906c90e9e1dad5def
seoAreum <1971236@hansung.ac.kr>
2024-06-06 16:07:32 +0900

    style: 공통 스타일 정의

---
+++ src/style/style.d.ts
@@ -0,0 +1,20 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primeBlue: string;
+      white: string;
+      lightGray: string;
+      darkGray: string;
+      black: string;
+    };
+
+    fonts: {
+      h2: SerializedStyles;
+      semibold_14: SerializedStyles;
+      regular_14: SerializedStyles;
+      medium_12: SerializedStyles;
+    };
+  }
+}
---
+++ src/style/theme.ts
@@ -0,0 +1,45 @@
+import { css, DefaultTheme } from 'styled-components';
+
+const colors = {
+  primeBlue: 'rgb(0, 149, 246)',
+  white: 'rgb(255, 255, 255)',
+  lightGray: 'rgb(239, 239, 239)',
+  darkGray: 'rgb(115, 115, 115)',
+  black: 'rgb(0, 0, 0)',
+};
+
+const fonts = {
+  h2: css`
+    font-family: 'Pretendard';
+    font-size: 2rem;
+    font-weight: 400;
+    line-height: 2.5rem;
+  `,
+
+  semibold_14: css`
+    font-family: 'Pretendard';
+    font-size: 1.4rem;
+    font-weight: 600;
+    line-height: 1.8rem;
+  `,
+
+  regular_14: css`
+    font-family: 'Pretendard';
+    font-size: 1.4rem;
+    font-weight: 400;
+    line-height: 1.8rem;
+  `,
+
+  medium_12: css`
+    font-family: 'Pretendard';
+    font-size: 1.2rem;
+    font-weight: 500;
+    line-height: 1.6rem;
+  `,
+};
+
+const theme: DefaultTheme = {
+  colors,
+  fonts,
+};
+export default theme;
