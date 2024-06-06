5a5623efe973f282f3675ec9dd50d0fd09b5b41b
seoAreum <1971236@hansung.ac.kr>
2024-06-06 17:45:56 +0900

    style: svgr 세팅

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite-plugin-svgr/client" />
+import IcMore from './icon/ic_more.svg?react';
+import IcRecommend from './icon/ic_recommend.svg?react';
+import IcSelect from './icon/ic_select.svg?react';
+
+export { IcMore, IcRecommend, IcSelect };
