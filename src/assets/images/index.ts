c4bbdd1e17d14215d241541aa16117fe893a4bde
wrryu09 <rryu09@ewhain.net>
2024-06-09 02:41:54 +0900

    style: 추천프로필 이미지 추가

---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+import rec_profile_1 from './rec_profile_1.png';
+import rec_profile_2 from './rec_profile_2.png';
+
+const images = {
+  rec_profile_1,
+  rec_profile_2,
+};
+
+export default images;
