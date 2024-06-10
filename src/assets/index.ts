e1214e4adb42f13baa327522ba616be25ae5c156
wrryu09 <rryu09@ewhain.net>
2024-06-10 15:00:13 +0900

    style: 모달 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,19 @@
 /// <reference types="vite-plugin-svgr/client" />
 
 import IcClose from './icon/ic_close.svg?react';
+import IcCloseFriend from './icon/ic_closefriend.svg?react';
+import IcFavorite from './icon/ic_favorite.svg?react';
+import IcModalClose from './icon/ic_modal_close.svg?react';
 import IcMore from './icon/ic_more.svg?react';
 import IcRecommend from './icon/ic_recommend.svg?react';
 import IcSelect from './icon/ic_select.svg?react';
 
-export { IcMore, IcRecommend, IcSelect, IcClose };
+export {
+  IcMore,
+  IcRecommend,
+  IcSelect,
+  IcClose,
+  IcCloseFriend,
+  IcFavorite,
+  IcModalClose,
+};
