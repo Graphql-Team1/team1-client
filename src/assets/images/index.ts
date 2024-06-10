5117682b8c032420d91a74bafdc212940d2e2482
wrryu09 <rryu09@ewhain.net>
2024-06-10 11:55:57 +0900

    style: 프로필 이미지 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,9 +1,17 @@
 import rec_profile_1 from './rec_profile_1.png';
 import rec_profile_2 from './rec_profile_2.png';
+import rec_profile_3 from './rec_profile_3.png';
+import rec_profile_4 from './rec_profile_4.png';
+import rec_profile_5 from './rec_profile_5.png';
+import rec_profile_6 from './rec_profile_6.png';
 
 const images = {
   rec_profile_1,
   rec_profile_2,
+  rec_profile_3,
+  rec_profile_4,
+  rec_profile_5,
+  rec_profile_6,
 };
 
 export default images;
