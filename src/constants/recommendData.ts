be189efa0108d08466bbaae145a52d83c96da82a
wrryu09 <rryu09@ewhain.net>
2024-06-10 11:59:35 +0900

    feat: 추천 더미프로필 데이터 추가

---
+++ src/constants/recommendData.ts
@@ -0,0 +1,28 @@
+import images from '../assets/images';
+
+const DUMMYRECOMMDATA = [
+  { name: 'nasa', desc: 'NASA', img: images.rec_profile_1 },
+  {
+    name: 'iss',
+    desc: 'International Space Station',
+    img: images.rec_profile_2,
+  },
+  { name: 'natgeo', desc: 'National Geographic', img: images.rec_profile_3 },
+  {
+    name: 'earth',
+    desc: 'Earth',
+    img: images.rec_profile_4,
+  },
+  {
+    name: 'nasawebb',
+    desc: 'NASA Webb Telescope',
+    img: images.rec_profile_5,
+  },
+  {
+    name: 'zuck',
+    desc: 'Mark Zuckerberg',
+    img: images.rec_profile_6,
+  },
+];
+
+export default DUMMYRECOMMDATA;
